dba130167716fe4d99e34ae61f03bdb12fe69a00
배세은 <baeseeun@baeseeun-ui-MacBookAir.local>
2024-07-05 18:13:22 +0900

    Create package

---
+++ src/pack/PakageMain2.java
@@ -0,0 +1,12 @@
+package pack;
+
+import pack.a.User;
+
+public class PakageMain2 {
+
+    public static void main(String[] args) {
+        Data data = new Data();
+
+        User user = new User();
+    }
+}
